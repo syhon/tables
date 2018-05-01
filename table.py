9521e4cb0fc478a807a4e65e78d182420a6fb5fd
Lars <lars.kolk@tu-dortmund.de>
2018-05-01 19:13:09 +0200

    sollte jetzt auch 1-spaltige Tabellen machen.

--- table.py
+++ table.py
@@ -4,6 +4,8 @@ datainput=input()
 datafile=datainput+".txt"
 datatex=datainput+".tex"
 while True:
+   j=2
+
    array=[]
    i=0
    data=open(datafile, "r")
@@ -27,38 +29,53 @@ while True:
    table.write("\\begin{table}[H] \n   \centering \n   \caption{name} \n   \label{tab:name} \n   \\begin{tabular} ")
    i=0
    table.write("{ ")
-   while i<len(data):
-       table.write("c ")
-       i+=1
-   table.write("} \n \\toprule \n ")
-   i=0
-   while i < len(data):
+   if len(np.shape(data)) >1:
+      while i<len(data):
+         table.write("c ")
+         i+=1
+      table.write("} \n \\toprule \n ")
+      i=0
+      while i < len(data):
+          table.write("{$")
+          table.write(data[i][0])
+          table.write("\:/\: \mathrm{")
+          table.write(data[i][1])
+          table.write("}$}")
+          if i+1 ==len(data):
+              table.write(" \\\ \n")
+          else:
+              table.write(" & ")
+          i+=1
+      table.write("    \midrule \n")
+      j=2
+      while j < len(data[0]):
+           i=0
+           table.write("    ")
+           while i < len(data):
+               table.write(data[i][j])
+               if i+1 ==len(data):
+                   table.write(" \\\ \n")
+               else:
+                   table.write(" & ")
+               i+=1
+           j+=1
+
+   else:
+       table.write("c } \n \\toprule \n ")
        table.write("{$")
        table.write(data[i][0])
        table.write("\:/\: \mathrm{")
        table.write(data[i][1])
        table.write("}$}")
-       if i+1 ==len(data):
+       table.write(" \\\ \n")
+       table.write("    \midrule \n")
+       while j < len(data):
+           table.write("    ")
+           table.write(data[j])
            table.write(" \\\ \n")
-       else:
-           table.write(" & ")
-       i+=1
-   table.write("    \midrule \n")
-   j=2
-   while j < len(data[0]):
-       i=0
-       table.write("    ")
-       while i < len(data):
-           table.write(data[i][j])
-           if i+1 ==len(data):
-               table.write(" \\\ \n")
-           else:
-               table.write(" & ")
-           i+=1
-       j+=1
+           j+=1
    table.write("    \\bottomrule \n  \end{tabular}\n\end{table}")
    table.close()
-
    i=0
    data=open(datafile, "r")
    for line in data:
